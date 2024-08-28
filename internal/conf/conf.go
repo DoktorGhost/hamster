1052d9d26417f7a3e306eec05ad28c8207a9ec49
Oleg Samsonov <ghostkote@gmail.com>
2024-08-28 16:10:02 +0300

    add new game

--- internal/conf/conf.go
+++ internal/conf/conf.go
@@ -1,7 +1,7 @@
 package conf
 
 // при добвлении новой игры CountGames++, в MapToken добавляем данные новой игры
-const CountGames = 8
+const CountGames = 9
 
 var MapToken = map[int]Token{
 	0: {"Riding Extreme 3D", "d28721be-fd2d-4b45-869e-9f253b554e50", "43e35910-c168-4634-ad4f-52fd764a843f"},
@@ -12,6 +12,7 @@ var MapToken = map[int]Token{
 	5: {"Twerk Race", "61308365-9d16-4040-8bb0-2f4a4c69074c", "61308365-9d16-4040-8bb0-2f4a4c69074c"},
 	6: {"Polysphere", "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71", "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71"},
 	7: {"Mow and Trim", "ef319a80-949a-492e-8ee0-424fb5fc20a6", "ef319a80-949a-492e-8ee0-424fb5fc20a6"},
+	8: {"Mow and Trim", "bc0971b8-04df-4e72-8a3e-ec4dc663cd11", "bc0971b8-04df-4e72-8a3e-ec4dc663cd11"},
 }
 
 type Token struct {
